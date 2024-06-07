80e0f4779c3f6dd8db722d17d38992134a06ef75
CopperCableIsolator <franzisco.mail@web.de>
2024-06-07 15:36:23 +0200

    added some stupid coloring, will delete later

--- prompts.py
+++ prompts.py
@@ -1,9 +1,10 @@
+from colorama import Fore, Back, Style
 
-internalError = '''An internal Error occured. Restart the program or try again. Error signature: '''
+internalError = Fore.RED + '''An internal Error occured. Restart the program or try again. Error signature: ''' + Style.RESET_ALL
 
 lineSeperator = '''-----------------------------------------------------------------------------C'''
 
-linePrompt = '''>>> '''
+linePrompt = Back.LIGHTBLUE_EX + Fore.BLACK + '''>>> ''' + Style.RESET_ALL
 
 abortKeyword = '''abort'''
 
@@ -17,26 +18,26 @@ contentPrompt = '''content='''
 
 deadlinePrompt = '''deadline='''
 
-initWarning = '''WARNING THIS WILL DELETE ALL YOUR NOTES!!! DO YOU WANT TO CONTINUE?'''
+initWarning = Fore.RED + '''WARNING THIS WILL DELETE ALL YOUR NOTES!!! DO YOU WANT TO CONTINUE?''' + Style.RESET_ALL
 
 typeConfirmWarning = '''Type <CONFIRM> to confirm the operation'''
 
-commandNotRecognized = '''Command not recognized! type <help> for more'''
+commandNotRecognized = Fore.LIGHTRED_EX + '''Command not recognized! type <help> for more''' + Style.RESET_ALL
 
 helpMessage = '''
-    This is the todolist program writen by Franzisco Schmidt 
+    This is the todl program writen by Franzisco Schmidt 
 
     It is used to store and handle very simple todo-list actions through a command
     line interface. Use commands to interface with the program:
 
     <help> - prints this message
-    <init files> - writes the files 'done' and 'todo' new if they dont exist
+    <init files> - writes the files 'done' and 'todo' new
     <list all> - lists all entries in both files
     <list done> - lists entries in 'done'
     <list todo> - lists entries in 'todo'
     <insert> - insert an item into the list (this will open another prompt)
-    <did> + param - change the status of the entry with id=param to done
-    <undid> + param - change the status of the entry with id=param to todo
+    <did> - change the status of the entry with id=param to done (this will open another prompt)
+    <undid> - change the status of the entry with id=param to todo (this will open another prompt)
     <delete done> - removes all entries in 'done'
     <exit> - closes the program
 
